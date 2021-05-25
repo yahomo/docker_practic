a938d93dea51ea86fe4f1b63a6a4fd0cf27f9cc7
jan <jan@desktop-vei47a9>
2021-05-25 10:06:57 -0600

    new commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const router = express.Router();
+
+//Settingd
+//app.set('port', process.env.PORT || 3000);
+
+//Listening
+app.listen(3000);
+console.log('server on port', 3000);
+
+
+//routes
+const users = [
+    {
+        name: 'json',
+        age: 27
+    },
+    {
+      name:  'Carlos',
+      age: 35
+    },
+    {
+        name: 'pedro',
+        age: 25
+    }
+]
+
+const usersRoute = router.get('/users', (req, res) => {
+    res.json(users)
+});
+
+app.use(express.json());
+app.use('/ominey', usersRoute);
+
